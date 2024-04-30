60b4b846170f9710aba483d3d33c183456a32d0a
Daniel Whatley <daniel@govividly.com>
2024-04-30 15:21:32 -0700

    more changes

--- src/builder-registry.ts
+++ src/builder-registry.ts
@@ -19,8 +19,79 @@ Builder.registerComponent(WorldMap, {
   name: "World Map",
   inputs: [
     {
-      name: "locationsJson",
-      type: "text",
+      name: "locations",
+      type: "list",
+      defaultValue: [],
+      subFields: [
+        {
+          name: "markerOffset",
+          type: "number",
+          defaultValue: -20,
+        },
+        {
+          name: "name",
+          type: "text",
+          defaultValue: "Unnamed Location",
+        },
+        {
+          name: "coordinates",
+          type: "object",
+          defaultValue: {x: 0, y: 0},
+          subFields: [
+            {
+              name: "x",
+              type: "number",
+              required: true,
+            },
+            {
+              name: "y",
+              type: "number",
+              required: true,
+            }
+          ]
+        },
+        {
+          name: "information",
+          type: "longText",
+          defaultValue: "No information available yet.",
+        },
+        {
+          name: "link",
+          type: "text",
+        },
+        {
+          name: "size",
+          type: "number",
+          defaultValue: 15,
+        },
+        {
+          name: "color",
+          type: "text",
+          defaultValue: "#E42A1D",
+        },
+        {
+          name: "stroke",
+          type: "text",
+          defaultValue: "#FFF",
+        },
+        {
+          name: "strokeWidth",
+          type: "number",
+          defaultValue: 2,
+        },
+      ]
     },
   ],
 });
+
+// interface Marker {
+//   markerOffset: number;
+//   name: string;
+//   coordinates: number[];
+//   information?: string;
+//   link?: string;
+//   size?: number;
+//   color?: string;
+//   stroke?: string;
+//   strokeWidth?: number;
+// }
--- src/components/WorldMap/WorldMap.tsx
+++ src/components/WorldMap/WorldMap.tsx
@@ -13,21 +13,24 @@ import allStates from "./allstates.json";
 
 const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
 
-interface WorldMapProps {
-  locationsJson?: string;
-}
+const X_LOWER_BOUND = -125, X_UPPER_BOUND = -68;
+const Y_LOWER_BOUND = 25, Y_UPPER_BOUND = 80;
 
 // TODO: Allow this to be fetched from an api, or maybe google sheet
-interface Marker {
+interface InputMarker {
   markerOffset: number;
   name: string;
-  coordinates: number[];
+  coordinates: {x: number, y: number};
   information?: string;
   link?: string;
   size?: number;
   color?: string;
   stroke?: string;
   strokeWidth?: number;
+}
+
+interface WorldMapProps {
+  locations?: InputMarker[];
 }
 
 const useMousePosition = () => {
@@ -48,31 +51,25 @@ const useMousePosition = () => {
 };
 
 
-const markers = [
-  { markerOffset: -20, name: "Chicago", coordinates: [-87.6298, 41.8781] },
-  { markerOffset: -20, name: "Boston", coordinates: [-71.0589, 42.3601] },
-  { markerOffset: -20, name: "Tulsa", coordinates: [-95.9928, 36.154] },
-  { markerOffset: -20, name: "Baltimore", coordinates: [-76.6122, 39.2904] },
-  { markerOffset: -20, name: "Miami", coordinates: [-80.1918, 25.7617] },
+const markers: InputMarker[] = [
+  { markerOffset: -20, name: "Chicago", coordinates: {x: -87.6298, y: 41.8781} },
+  { markerOffset: -20, name: "Boston", coordinates: {x: -71.0589, y: 42.3601} },
+  { markerOffset: -20, name: "Tulsa", coordinates: {x: -95.9928, y: 36.154} },
+  { markerOffset: -20, name: "Baltimore", coordinates: {x: -76.6122, y: 39.2904} },
+  { markerOffset: -20, name: "Miami", coordinates: {x: -80.1918, y: 25.7617} },
   {
     markerOffset: 30,
     name: "Washington, D.C.",
-    coordinates: [-77.0369, 38.9072],
+    coordinates: {x: -77.0369, y: 38.9072},
   },
-  { markerOffset: -20, name: "Los Angeles", coordinates: [-118.2426, 34.0549] },
+  { markerOffset: -20, name: "Los Angeles", coordinates: {x: -118.2426, y: 34.0549} },
 ];
 
 const MapChart = (props: WorldMapProps) => {
-  const markersToUse: Marker[] = useMemo(() => {
-    try {
-      return props.locationsJson ? JSON.parse(props.locationsJson) : markers;
-    } catch (e) {
-      return [];
-    }
-  }, [props.locationsJson]);
+  const markersToUse: InputMarker[] = (props.locations ?? []);
   const mousePosition = useMousePosition();
   const [tooltipVisible, setTooltipVisible] = useState(false);
-  const [currentMarker, setCurrentMarker] = useState<Marker | undefined>(undefined);
+  const [currentMarker, setCurrentMarker] = useState<InputMarker | undefined>(undefined);
 
   return (
     <>
@@ -88,7 +85,7 @@ const MapChart = (props: WorldMapProps) => {
       {markersToUse.map((marker, index) => (
         <Marker
           key={index}
-          coordinates={marker.coordinates}
+          coordinates={[marker.coordinates.x, marker.coordinates.y]}
           id={marker.name}
           onMouseOver={() => {
             setTooltipVisible(true);
@@ -127,7 +124,6 @@ const MapChart = (props: WorldMapProps) => {
     }}>
       <b>{currentMarker?.name}</b><br />
       {currentMarker?.information ?? "No information available yet."}
-      {currentMarker?.link && (<div><br /><a href={currentMarker?.link}/></div>)}
     </div>
     </>
   );
